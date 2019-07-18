2e12e699df9fe0f7b0482d28d98be4b293f6dc64
Katie Lewis <klewi314@gmail.com>
2019-07-17 21:26:07 -0700

    Created routes for API

---
+++ routes/index.js
@@ -0,0 +1,3 @@
+module.exports = app => {
+  require('./recipeRoutes.js')(app)
+}
---
+++ routes/recipeRoutes.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+
+const { Recipes } = require('../models')
+
+module.exports = app => {
+
+  //Get drink by name search
+  app.get('/search/:name', (req, res) => {
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.params.name}`)
+      .then(({ data }) => console.log(data))
+      .catch(e => console.log(`ERROR IS ---------- ${e}`))
+  })
+
+  //Get ingredient by name 
+  app.get('/search/:ingredient', (req, res) => {
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${req.params.ingredient}`)
+      .then(({ data }) => console.log(data))
+      .catch(e => console.log(`ERROR IS ---------- ${e}`))
+  })
+
+
+  //Get a random cocktail
+  app.get('/random', (req, res) => {
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+      .then(({ data }) => console.log(data))
+      .catch(e => console.log(`ERROR IS ---------- ${e}`))
+  })
+
+}

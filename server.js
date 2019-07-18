bb8bc8b009409bb69cdbe21501a6cee94bd57d6b
Katie Lewis <klewi314@gmail.com>
2019-07-17 19:30:19 -0700

    Initial upload server finished

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { join } = require('path')
+const app = express()
+require('dotenv').config()
+
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/cocktailsdb'
+
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// require('./routes')(app)
+
+require('mongoose').connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+  .then(_ => app.listen(process.env.PORT || 8080))
+  .catch(e => console.log(e))

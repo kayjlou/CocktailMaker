0de82118a7664d995d397435d85259779b4523d9
Katie Lewis <klewi314@gmail.com>
2019-07-18 20:46:29 -0700

    Passed functions down to children

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Cards from './components/Cards'
+import SearchBar from './components/SearchBar'
 
 
 class App extends Component {
+  state = {
+    ingredient: '',
+    name: '',
+    favorites: []
+
+  }
+
+  handleSearchSubmit = () => {
+    console.log('running search')
+  }
   render() {
     return (
-      <Cards />
+      <>
+        <SearchBar handleSearchSubmit={this.handleSearchSubmit} />
+        <Cards />
+      </>
 
     )
   }

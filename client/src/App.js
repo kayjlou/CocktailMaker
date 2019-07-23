dddb0396f1fd24f0aa11d7045cb742ca755a1d99
Katie Lewis <klewi314@gmail.com>
2019-07-22 21:38:09 -0700

    all axios requests working

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Cards from './components/Cards'
 import SearchBar from './components/SearchBar'
 import Recipes from './components/assets/Utils/recipes.js'
+import axios from 'axios'
 
 
 class App extends Component {
   state = {
     ingredient: '',
-    name: 'margarita',
-    favorites: []
+    name: 'Caribbean Boilermaker',
+    favorites: [],
+    image: 'https://www.thecocktaildb.com/images/media/drink/svsxsv1454511666.jpg'
 
   }
 
   handleNameSearch = () => {
     console.log('running name search')
-    Recipes.getByName(this.state.name)
+    // Recipes.getByName(this.state.name)
+    axios.get('/random').then(({ data }) => {
+      console.log(`we getting random drinks ${data.drinks}`)
+    })
   }
   handleIngredientSearch = () => {
     console.log('running ingredient search')
     Recipes.getByIngredient()
   }
   handleRandomSearch = () => {
-    console.log('random search')
+    console.log('Running random')
     Recipes.getRandom()
+      .then(({ data }) => {
+        this.setState({
+          name: data.drinks[0].strDrink,
+          image: data.drinks[0].strDrinkThumb
+        })
+      })
+
+
+
   }
 
 
@@ -34,7 +47,8 @@ class App extends Component {
         <SearchBar handleIngredientSearch={this.handleIngredientSearch}
           handleNameSearch={this.handleNameSearch}
           handleRandomSearch={this.handleRandomSearch} />
-        <Cards />
+        <Cards name={this.state.name} image={this.state.image} />
+        <h4> {this.state.name}</h4>
       </>
 
     )

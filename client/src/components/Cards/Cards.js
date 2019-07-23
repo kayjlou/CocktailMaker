2bf3fc4c4f733be72b904e2ef23ba8e381f76ef3
Katie Lewis <klewi314@gmail.com>
2019-07-22 21:39:41 -0700

    added image and title to card from props

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardHeader,
@@ -12,19 +12,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
 
 
-export default function BasicCardExample() {
-  return (
-    <div className='container'>
-      <Card style={{ maxWidth: "300px" }}>
-        <CardHeader>Card header</CardHeader>
-        <CardImg src="https://place-hold.it/300x200" />
-        <CardBody>
-          <CardTitle>Lorem Ipsum</CardTitle>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <Button>Read more &rarr;</Button>
-        </CardBody>
-        <CardFooter>Card footer</CardFooter>
-      </Card>
-    </div>
-  );
+const imageStyle = {
+  width: "300px",
+  height: "auto"
+}
+export default class Cards extends Component {
+  render() {
+    const { name, image } = this.props
+    return (
+      <div className='container'>
+        <Card style={{ maxWidth: "300px" }}>
+          <CardHeader>Drink of the day</CardHeader>
+          <CardImg src={image} style={imageStyle} />
+          <CardBody>
+            <CardTitle>{name}</CardTitle>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <Button>Read more &rarr;</Button>
+          </CardBody>
+          <CardFooter>Card footer</CardFooter>
+        </Card>
+      </div>
+    );
+  }
 }
